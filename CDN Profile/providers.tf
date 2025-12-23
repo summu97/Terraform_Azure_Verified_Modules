ec211322c28b3134a290f63a34dc66855cc365ad
summu97 <135579543+summu97@users.noreply.github.com>
2025-12-23 19:05:30 +0530

    Update providers.tf

--- CDN Profile/providers.tf
+++ CDN Profile/providers.tf
@@ -1,22 +1,8 @@
 terraform {
-  required_version = ">= 1.9, < 2.0"
-
   required_providers {
-    azapi = {
-      source  = "Azure/azapi"
-      version = "~> 2.4"
-    }
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 4.0"
-    }
-    modtm = {
-      source  = "azure/modtm"
-      version = "~> 0.3"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = ">= 3.5.0"
     }
   }
 }
@@ -27,7 +13,4 @@ provider "azurerm" {
   use_msi                         = true
   subscription_id                 = "d629b553-466f-4caa-b64b-9ba2bae97c3f"
   resource_provider_registrations = "none"
-}
-provider "azapi" {
-  # Optional: subscription_id = "your-subscription-id"
 }
